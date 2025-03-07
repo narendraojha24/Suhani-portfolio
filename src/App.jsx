cff6267ca5b9295a8350727b10f549ebe54fffa6
suhani <suhani9620@gmail.com>
2025-03-07 23:21:20 +0530

    completed about component and start working on mywork component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/NavBar/Navbar'
 import Hero from './Components/NavBar/Hero/Hero'
 import About from './Components/NavBar/About/About'
+import MyWork from './Components/NavBar/MyWork/MyWork'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero/>
       <About/>
+      <MyWork/>
     </div>
   )
 }
